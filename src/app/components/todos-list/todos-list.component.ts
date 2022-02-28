6750e700a7ff48227d84b920307fe04de828a49e
diopmorfall <morfall.papydiop99@gmail.com>
2022-02-28 14:58:03 +0100

    Observable operator exercises

--- src/app/components/todos-list/todos-list.component.ts
+++ src/app/components/todos-list/todos-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { combineLatest, forkJoin, Subscription } from 'rxjs';
+import { filter, take, first, toArray, switchMap, distinct, map, tap } from 'rxjs/operators';
 import { Todo } from 'src/app/models/todo';
 import { TodoService } from 'src/app/services/todo.service';
 
@@ -17,9 +18,64 @@ export class TodosListComponent implements OnInit, OnDestroy {
     //todo: method that changes the state of the todo
 
     ngOnInit(): void {
-        this._subscriptions.push(
-            this._todoService.getTodos().subscribe(todos => this.todos = todos)
-        );
+
+        //forkJoin
+        // this._subscriptions.push(
+        //     this._todoService.getTodos().pipe(
+        //         switchMap(todos => todos), //? kinda starts a cycle, where I do some operations
+        //         filter(todo => todo.title.length > 60),
+        //         distinct(todo => todo.id),
+        //         toArray(), //? use it at the end of switchMap, to get all the results in an array
+        //     ).subscribe(todos => this.todos = todos)
+        // );
+
+        // forkJoin([
+        //     //* I'm taking the final value that each observer returns, 
+        //     this._todoService.getTodos().pipe(
+        //         switchMap(todos => todos),
+        //         filter(todo => todo.title.length > 30),
+        //         toArray(),
+        //     ),
+        //     this._todoService.getTodos().pipe(
+        //         switchMap(todos => todos),
+        //         filter(todo => todo.title.length > 50),                
+        //         toArray()
+        //     ),
+        //     this._todoService.timer().pipe(tap(res => console.log('TIMER' ,res)))
+        // ]).pipe(
+        //     tap(([res1, res2, timer]) => console.log('timer', timer)),
+        //     map(([res1, res2]) => res1.concat(res2)),
+        //     tap(todos => console.log('original todos', todos)),
+        //     switchMap(res => res),
+        //     distinct(todo => todo.id),
+        //     toArray(),
+        //     tap(todos => console.log('filtered todos', todos)), 
+        //     first()//? does a generic operation            
+        // ).subscribe(todos => this.todos = todos );
+
+        forkJoin([
+            this._todoService.timer().pipe(tap(res => console.log('TIMER', res))),
+            this._todoService.getTodos().pipe(
+                tap(todos => console.log('todos', todos)),
+                switchMap(todos => todos),
+                filter(todo => todo.title.length > 30),
+                toArray(),
+                switchMap(todos => this._todoService.getByTodoId(todos[0].id))
+            )
+        ]).pipe(
+            tap(todos => console.log('forkJoin', todos)),
+            switchMap(([timer, todo]) => this._todoService.updateById({timer: timer, id: todo.id}))
+        ).subscribe(todo => console.log('DONE', todo))
+
+        // this._todoService.getTodos().pipe(
+        //     tap(todos => console.log('todos', todos)),
+        //     switchMap(todos => todos),
+        //     filter(todo => todo.title.length > 30),
+        //     toArray(),
+        //     switchMap(todos => this._todoService.getByTodoId(todos[0].id))
+        // ).subscribe(todo => console.log('DONE', todo))
+
+        // const subscribe = join.subscribe(response => console.log(response));
     }
 
     ngOnDestroy(): void {
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { interval, Observable, of } from 'rxjs';
+import { map, filter, tap, switchMap , toArray, distinct, take} from 'rxjs/operators';
 import { TodosListComponent } from '../components/todos-list/todos-list.component';
 import { ITodo } from '../interfaces/todo.interface';
 import { Todo } from '../models/todo';
@@ -13,10 +13,40 @@ export class TodoService {
 
     constructor(private _http: HttpClient) { }
 
+    timer(): Observable<number>{
+        return interval(1000).pipe(take(5))
+    }
+
+    getByTodoId(id: number):Observable<{id:number}>{
+        console.log('getByTodoId', id)
+        return of({id: 123})
+    }
+
+    updateById(payload: { id: number, timer: number}):Observable<any>{
+        console.log('updateById', payload)
+        return of('pippo')
+    }
+
     getTodos(): Observable<Todo[]> {
         return this._http.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos')
         .pipe(
-            map(todos => todos.map(todo => new Todo(todo.userId, todo.id, todo.title, todo.isCompleted)))
+            map(todos => todos.map(todo => new Todo(todo.userId, todo.id, todo.title, todo.isCompleted))),
+            // map(todos => todos.filter(todo => todo.title.length > 50)),
+            // map(todos => {
+            //     let tmp: Todo[] = []
+            //     todos.forEach(todo => {
+            //         if(tmp.indexOf(todo) === -1){
+            //             tmp.push(todo)
+            //         }
+            //     })
+            //     return tmp
+            // }),
+            // switchMap(todos => todos), //? kinda starts a cycle, where I do some operations
+            // filter(todo => todo.title.length > 50),
+            // distinct(todo => todo.id),
+            // toArray(), //? use it at the end of switchMap, to get all the results in an array
+            
+            //* if each component needs different data, I can filter it in each one of them
         );
     }
 
